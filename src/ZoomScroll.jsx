2a55de752c1f3a912a85efb85009c06d1c72d2c1
Tom Grant <grntthomasronnie@gmail.com>
2023-07-18 15:49:04 +0100

    Added horizontal scroll moving

--- src/ZoomScroll.jsx
+++ src/ZoomScroll.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import './ZoomScroll.css';
 
 const ZoomScroll = () => {
-  const frog = () => {
+  const frog = (e) => {
     let move = window.getComputedStyle(document.documentElement).getPropertyValue('--move');
-    let newMove = parseInt(move) + 10;
+    let newMove = 0;
+    if (e.deltaY > 0){
+        newMove = parseInt(move) + 20; // number is for how many px to move
+    } else {
+        newMove = parseInt(move) - 20;
+    }
     document.documentElement.style.setProperty('--move', `${newMove}px`);
   };
 
   return (
     <>
+    <div onWheel={frog} className="scroll-area">
       <div className="center slide">
         <h1>This is floating</h1>
         <button onClick={frog}>Press</button>
       </div>
+    </div>
     </>
   );
 };
